13a8ab1f20a07d30935c32766a7bfca8c8406c0b
Mauro Bonaldo <mauro.bonaldo@emme-bi.com>
2021-05-26 18:42:09 +0200

    Update mistakes

--- src/config/sharing-linkedin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'redirect_uri'     => env('SHARING_LINKEDIN_REDIRECT_URI', ''),
-    'client_id'        => env('SHARING_LINKEDIN_CLIENT_ID', ''),
-    'client_secret'    => env('SHARING_LINKEDIN_CLIENT_SECRET', ''),
-
-];
