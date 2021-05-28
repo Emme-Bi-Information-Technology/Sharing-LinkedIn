4c85430b379c268a74fe4b13a7925dff3370d68a
Mauro Bonaldo <mauro.bonaldo@emme-bi.com>
2021-05-28 11:23:06 +0200

    Automatic update

--- src/Facades/SharingLinkedin.php
+++ src/Facades/SharingLinkedin.php
@@ -2,13 +2,10 @@
 
 namespace EmmeBi\SharingLinkedin\Facades;
 
-use EmmeBi\SharingLinkedin;
-
-
 class SharingLinkedin extends \Illuminate\Support\Facades\Facade
 {
     protected static function getFacadeAccessor()
     {
-        return EmmeBi\SharingLinkedin\SharingLinkedin::class;
+        return \EmmeBi\SharingLinkedin\SharingLinkedin::class;
     }
 }
